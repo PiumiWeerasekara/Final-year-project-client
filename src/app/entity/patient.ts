89200ba920ac0599f80032d1ffb4e94df1f75beb
biyankaPiumi <developer04.softwarestylers@gmail.com>
2024-07-13 14:10:50 +0530

    patient room

---
+++ src/app/entity/patient.ts
@@ -0,0 +1,45 @@
+import {Gender} from "./gender";
+import {Specialization} from "./specialization";
+// import {Designation} from "./designation";
+// import {Empstatus} from "./empstatus";
+// import {Emptype} from "./emptype";
+
+export class Doctor{
+
+  public id !: number;
+  public title !: string;
+  public firstName !: string;
+  public lastName !: string;
+  public photo !: string;
+  public dob !: string;
+  public nic !: string;
+  public address !: string;
+  public contactNo !: string;
+  public email !: string;
+  public gender !: Gender;
+  public speciality !: Specialization;
+  public  medicalLicenseNo !: string;
+  public  licenseEXPDate !: string;
+
+
+  constructor(id: number, title: string, firstName: string, lastName: string, photo: string, dob: string, nic: string, address: string, contactNo: string, email: string, gender: Gender, speciality: Specialization, medicalLicenseNo: string, licenseEXPDate: string) {
+    this.id = id;
+    this.title = title;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.photo = photo;
+    this.dob = dob;
+    this.nic = nic;
+    this.address = address;
+    this.contactNo = contactNo;
+    this.email = email;
+    this.gender = gender;
+    this.speciality = speciality;
+    this.medicalLicenseNo = medicalLicenseNo;
+    this.licenseEXPDate = licenseEXPDate;
+  }
+}
+
+
+
+
---
+++ src/app/entity/room.ts
@@ -0,0 +1,38 @@
+import {Gender} from "./gender";
+export class Patient{
+
+  public id !: number;
+  public title !: string;
+  public firstName !: string;
+  public lastName !: string;
+  public photo !: string;
+  public dob !: string;
+  public nic !: string;
+  public address !: string;
+  public contactNo !: string;
+  public email !: string;
+  public gender !: Gender;
+  public guardianName !: string;
+  public guardianContactNo !: string;
+
+
+  constructor(id: number, title: string, firstName: string, lastName: string, photo: string, dob: string, nic: string, address: string, contactNo: string, email: string, gender: Gender, guardianName: string, guardianContactNo: string) {
+    this.id = id;
+    this.title = title;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.photo = photo;
+    this.dob = dob;
+    this.nic = nic;
+    this.address = address;
+    this.contactNo = contactNo;
+    this.email = email;
+    this.gender = gender;
+    this.guardianName = guardianName;
+    this.guardianContactNo = guardianContactNo;
+  }
+}
+
+
+
+
